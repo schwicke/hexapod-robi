6730e7aa9a4f989fb3b9b163be67284ab6b1359a
Ulrich Schwickerath <ulrich.schwickerath@cern.ch>
2021-08-16 11:53:54 +0200

    update sample walk.py for the new model and pylint it

--- software/walk.py
+++ software/walk.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" sample script to make it walk """
 #
 # This file is part of the hexapod-robi distribution (https://github.com/schwicke/hexapod-robi).
 # Copyright (c) 2021 Ulrich Schwickerath
@@ -15,127 +16,127 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-""" PoC move primitives """
+
+import sys
 import time
 #pylint: disable=no-member
 from pypot import dynamixel
 
-MOTORS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
-PORT = dynamixel.find_port(MOTORS)
+ALL_MOTORS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+MOTOR = dynamixel.io.io.DxlIO(dynamixel.find_port(ALL_MOTORS), use_sync_read=False)
 
-m = dynamixel.io.io.DxlIO(PORT, use_sync_read=False)
 #print(m.get_angle_limit(MOTORS))
 #print(m.get_control_mode(MOTORS))
 #print(m.get_firmware(MOTORS))
 #print(m.get_present_temperature(MOTORS))
-print(m.get_present_speed(MOTORS))
+print(MOTOR.get_present_speed(ALL_MOTORS))
 
 LEG_MOTOR_SPEED = [50, 50, 50]
 LEG_GROUPS = [[1, 3, 5], [2, 4, 6]]
 
 # define positions
-LEG_ZERO_POS = [0, -70, -70]
-LEG_UP_POS = [None, -80, -50]
-LEG_DOWN_POS = [None, -70, -70]
-LEG_FORWARD_POS = [10, None, None]
-LEG_BACKWARD_POS = [-10, None, None]
+LEG_ZERO_POS = [0, 0, 0]
+LEG_UP_POS = [None, 80, 80]
+LEG_DOWN_POS = [None, 0, 0]
+LEG_FORWARD_POS = [0, None, None]
+LEG_BACKWARD_POS = [-30, None, None]
+LEG_SIT_POS = [0, -55, 50]
 
 def _define_motor_hash(legs, positions):
     """ legs: list of legs, position: list of 3 values  """
-    mhash = {}
+    res_hash = {}
     for leg in legs:
         for i in range(3):
-            motor = 3*(leg-1)+i + 1
+            mindex = 3*(leg-1)+i + 1
             if positions[i] is not None:
-                if motor in [10, 13, 16] and i == 0:
-                    mhash[motor] = -positions[i]
+                if mindex in [10, 13, 16] and i == 0:
+                    res_hash[mindex] = -positions[i]
                 else:
-                    mhash[motor] = positions[i]
-    return mhash
+                    res_hash[mindex] = positions[i]
+    return hash
 
-leg_groups_speed = [_define_motor_hash(LEG_GROUPS[0], LEG_MOTOR_SPEED),
+LEG_GROUPS_SPEED = [_define_motor_hash(LEG_GROUPS[0], LEG_MOTOR_SPEED),
                     _define_motor_hash(LEG_GROUPS[1], LEG_MOTOR_SPEED)]
-leg_groups_zero = [_define_motor_hash(LEG_GROUPS[0], LEG_ZERO_POS),
+LEG_GROUPS_ZERO = [_define_motor_hash(LEG_GROUPS[0], LEG_ZERO_POS),
                    _define_motor_hash(LEG_GROUPS[1], LEG_ZERO_POS)]
-leg_groups_up = [_define_motor_hash(LEG_GROUPS[0], LEG_UP_POS),
+LEG_GROUPS_UP = [_define_motor_hash(LEG_GROUPS[0], LEG_UP_POS),
                  _define_motor_hash(LEG_GROUPS[1], LEG_UP_POS)]
-leg_groups_down = [_define_motor_hash(LEG_GROUPS[0], LEG_DOWN_POS),
+LEG_GROUPS_DOWN = [_define_motor_hash(LEG_GROUPS[0], LEG_DOWN_POS),
                    _define_motor_hash(LEG_GROUPS[1], LEG_DOWN_POS)]
-leg_groups_forward = [_define_motor_hash(LEG_GROUPS[0], LEG_FORWARD_POS),
+LEG_GROUPS_FORWARD = [_define_motor_hash(LEG_GROUPS[0], LEG_FORWARD_POS),
                       _define_motor_hash(LEG_GROUPS[1], LEG_FORWARD_POS)]
-leg_groups_backward = [_define_motor_hash(LEG_GROUPS[0], LEG_BACKWARD_POS),
+LEG_GROUPS_BACKWARD = [_define_motor_hash(LEG_GROUPS[0], LEG_BACKWARD_POS),
                        _define_motor_hash(LEG_GROUPS[1], LEG_BACKWARD_POS)]
+LEG_GROUPS_SIT = [_define_motor_hash(LEG_GROUPS[0], LEG_SIT_POS),
+                  _define_motor_hash(LEG_GROUPS[1], LEG_SIT_POS)]
 
-m.set_moving_speed(leg_groups_speed[0])
-m.set_moving_speed(leg_groups_speed[1])
+MOTOR.set_moving_SPEED(LEG_GROUPS_SPEED[0])
+MOTOR.set_moving_SPEED(LEG_GROUPS_SPEED[1])
 
-m.set_goal_position(leg_groups_zero[0])
-m.set_goal_position(leg_groups_zero[1])
+MOTOR.set_goal_position(LEG_GROUPS_ZERO[0])
+MOTOR.set_goal_position(LEG_GROUPS_ZERO[1])
 
-print(leg_groups_speed)
-print(m.get_moving_speed(MOTORS))
+print(LEG_GROUPS_SPEED)
+print(MOTOR.get_moving_SPEED(ALL_MOTORS))
 
-def zero(delay=0.5):
-    """ set two groups of legs to zero position """
-    m.set_goal_position(leg_groups_zero[0])
-    m.set_goal_position(leg_groups_zero[1])
+def zero():
+    """ set all MOTORS to zero """
+    MOTOR.set_goal_position(LEG_GROUPS_ZERO[0])
+    MOTOR.set_goal_position(LEG_GROUPS_ZERO[1])
 
 def set_speed(speed=50):
-    """ set two groups of legs to zero position """
+    """ set the motor speed for all groups """
     leg_groups_speed = [_define_motor_hash(LEG_GROUPS[0], [speed, speed, speed]),
                         _define_motor_hash(LEG_GROUPS[1], [speed, speed, speed])]
-    m.set_moving_speed(leg_groups_speed[0])
-    m.set_moving_speed(leg_groups_speed[1])
+    MOTOR.set_moving_speed(leg_groups_speed[0])
+    MOTOR.set_moving_speed(leg_groups_speed[1])
 
 def walk_forward(steps, delay=0.5, speed=50):
-    """ walk forward """
-    zero(delay)
+    """ walk forward by the defined number of steps """
+    set_speed(speed)
+    zero()
     for i in range(steps):
+        print("Step forward number: %d", i)
         for group in range(2):
-            m.set_goal_position(leg_groups_up[group])
+            MOTOR.set_goal_position(LEG_GROUPS_UP[group])
             time.sleep(delay)
-            m.set_goal_position(leg_groups_backward[1-group])
+            MOTOR.set_goal_position(LEG_GROUPS_BACKWARD[1-group])
             time.sleep(delay)
-            m.set_goal_position(leg_groups_forward[group])
+            MOTOR.set_goal_position(LEG_GROUPS_FORWARD[group])
             time.sleep(delay)
-            m.set_goal_position(leg_groups_down[group])
+            MOTOR.set_goal_position(LEG_GROUPS_DOWN[group])
             time.sleep(delay)
 
-def dance(steps, delay=0.5, speed=50):
-    """ some dancing but do not move """
-    zero(delay)
+def walk_backward(steps, delay=0.5, speed=50):#
+    """ walk backward by the defined number of steps """
+    set_speed(speed)
+    zero()
     for i in range(steps):
+        print("Step backward number: %d", i)
         for group in range(2):
-            m.set_goal_position(leg_groups_up[group])
+            MOTOR.set_goal_position(LEG_GROUPS_UP[group])
             time.sleep(delay)
-            m.set_goal_position(leg_groups_backward[group])
+            MOTOR.set_goal_position(LEG_GROUPS_FORWARD[1-group])
             time.sleep(delay)
-            m.set_goal_position(leg_groups_forward[group])
+            MOTOR.set_goal_position(LEG_GROUPS_BACKWARD[group])
             time.sleep(delay)
-            m.set_goal_position(leg_groups_down[group])
+            MOTOR.set_goal_position(LEG_GROUPS_DOWN[group])
             time.sleep(delay)
 
-def walk_backward(steps, delay=0.5, speed=50):
-    """ go backward by some steps """
-    zero(delay)
-    for i in range(steps):
-        for group in range(2):
-            m.set_goal_position(leg_groups_up[group])
-            time.sleep(delay)
-            m.set_goal_position(leg_groups_forward[1-group])
-            time.sleep(delay)
-            m.set_goal_position(leg_groups_backward[group])
-            time.sleep(delay)
-            m.set_goal_position(leg_groups_down[group])
-            time.sleep(delay)
+def main():
+    """ main entry point """
+    set_speed(80)
+    zero()
+    steps = 5
+    wait = 0.15
+    speed = 130
+    time.sleep(1)
+    walk_backward(steps, wait, speed)
+    #walk_forward(steps,  wait, speed)
+    set_speed(30)
+    MOTOR.set_goal_position(LEG_GROUPS_SIT[1])
+    MOTOR.set_goal_position(LEG_GROUPS_SIT[0])
+    #zero()
 
-set_speed(50)
-zero(0.1)
-steps = 5
-wait = 0.4
-speed = 100
-time.sleep(3)
-walk_forward(steps, wait, speed)
-dance(steps, wait, speed)
-walk_backward(steps, wait, speed)
-zero(0.1)
+if __name__ == '__main__':
+    sys.exit(main())
